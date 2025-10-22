2a6b3b47d5120883f3915861be24a7dcb3f92f85
keshew <artyom.korotckov2017@yandex.ru>
2025-10-22 14:38:39 +0500

    Initial Commit

---
+++ TravelMate/TravelMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelMateApp.swift
+//  TravelMate
+//
+//  Created by Артём Коротков on 22.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
